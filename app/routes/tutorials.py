ab1cc1a95754e1f17b18f001e5f407672b9d8dbc
Kambole21 <kambole520@gmail.com.com>
2025-08-06 18:56:27 +0200

    fixed bug

--- app/routes/tutorials.py
+++ app/routes/tutorials.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
 from app import db
 from bson.objectid import ObjectId
+from app.routes.login import login_required
 bp = Blueprint('tutorial', __name__)
 
 @bp.route('/Tutorial')
+@login_required
 def tut():
 	user_id = session.get('user_id')
 	user_data = None
